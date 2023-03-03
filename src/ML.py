3667fd0482970828b3cd73c5a100d565ab4ff9ee
NadaAbdellatef <61163866+nada320@users.noreply.github.com>
2023-03-03 03:50:17 -0800

    Update ML.py

--- src/ML.py
+++ src/ML.py
@@ -77,10 +77,10 @@ def predict(logr, X, threshold):
 def load_model(model_fname = './src/model/pickleee'):
   f = open(model_fname, 'rb')
  
-  logr = pickle.load(f)
+  logr = joblib.load(f)
   f.close()
   f = open(model_fname, 'rb')
-  threshold = pickle.load(f)
+  threshold = joblib.load(f)
   f.close()
   return logr, threshold
 
